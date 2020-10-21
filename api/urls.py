d5593123c72036b3c13ce006b9d19c9c2e1bab1c
tousif noor <tousif.noor@emumba.com>
2020-10-21 19:06:24 +0500

    Swaager URLs move to new file , redoc remove

--- api/urls.py
+++ api/urls.py
@@ -1,20 +1,10 @@
 from django.conf.urls import include
 from django.urls import path
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularRedocView, SpectacularSwaggerView)
+
+from api.swaggerurls import swaggerurls
 
 urlpatterns = [
     path("users/", include("api.users.urls")),
-    #OpenAPI sehema generation and swagger UI urls
-    path('api/schema/',
-         SpectacularAPIView.as_view(),
-         name='schema'),
-    # Optional UI:
-    path('api/schema/swagger-ui/',
-         SpectacularSwaggerView.as_view(url_name='schema'),
-         name='swagger-ui'),
-    path('api/schema/redoc/',
-         SpectacularRedocView.as_view(url_name='schema'),
-         name='redoc'),
 ]
+
+urlpatterns += swaggerurls
