3f3684755d653aad0c7de622cab1166e2dc1e898
tousif noor <tousif.noor@emumba.com>
2020-10-21 16:28:03 +0500

    SwaggerUI with auto OpenAPI schema generation

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,20 @@
 from django.conf.urls import include
 from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView, SpectacularSwaggerView)
 
 urlpatterns = [
     path("users/", include("api.users.urls")),
+    #OpenAPI sehema generation and swagger UI urls
+    path('api/schema/',
+         SpectacularAPIView.as_view(),
+         name='schema'),
+    # Optional UI:
+    path('api/schema/swagger-ui/',
+         SpectacularSwaggerView.as_view(url_name='schema'),
+         name='swagger-ui'),
+    path('api/schema/redoc/',
+         SpectacularRedocView.as_view(url_name='schema'),
+         name='redoc'),
 ]
