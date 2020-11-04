f869c758d2ab1650c01f56383256013c7f733500
zeeshan-emumba <zeeshan.iqbal@emumba.com>
2020-11-04 16:28:46 +0500

    remove verify token

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,10 +1,9 @@
+"""urls for users module"""
 from django.urls import path
-from rest_framework_jwt.views import verify_jwt_token
 from .views import LoginView, RefreshTokenView, UserSignUpAPIView
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="auth-login"),
     path("sign-up/", UserSignUpAPIView.as_view(), name="auth-login"),
     path("refresh-token/", RefreshTokenView.as_view(), name="refresh_jwt_token"),
-    path("verify-token/", verify_jwt_token),
 ]
