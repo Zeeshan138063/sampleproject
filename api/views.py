d80a88b3e7a8c74b6851f00742d5049bba4ca694
zeeshan-emumba <zeeshan.iqbal@emumba.com>
2020-11-05 15:35:32 +0500

    update celery branch and added tousif code for swagger

--- api/views.py
+++ api/views.py
@@ -14,14 +14,46 @@ class BaseAPIView(APIView):
     authentication_classes = (authentication.JSONWebTokenAuthentication,)
     permission_classes = (permissions.IsAuthenticated,)
 
+    def get_serializer_context(self):
+        """
+        Extra context provided to the serializer class.
+        """
+        return {
+            "request": self.request,
+            "view": self,
+        }
+
+    def get_serializer_class(self):
+        """
+        Return the class to use for the serializer.
+        Defaults to using `self.serializer_class`.
+        You may want to override this if you need to provide different
+        serializations depending on the incoming request.
+        (Eg. admins get full serialization, others get basic serialization)
+        """
+        assert self.serializer_class is not None, (
+            "'%s' should either include a `serializer_class` attribute, "
+            "or override the `get_serializer_class()` method." % self.__class__.__name__
+        )
+        return self.serializer_class
+
+    def get_serializer(self, *args, **kwargs):
+        """
+        Return the serializer instance that should be used for validating and
+        deserializing input, and for serializing output.
+        """
+        serializer_class = self.get_serializer_class()
+        kwargs["context"] = self.get_serializer_context()
+        return serializer_class(*args, **kwargs)
+
     def send_response(  # pylint: disable=no-self-use, bad-continuation, dangerous-default-value, too-many-arguments
-            self,
-            success=True,
-            code="",
-            status_code=status.HTTP_200_OK,
-            payload=None,
-            errors=None,
-            description="",
+        self,
+        success=True,
+        code="",
+        status_code=status.HTTP_200_OK,
+        payload=None,
+        errors=None,
+        description="",
     ):
         """
         Generates response.
