9233a6102ab4b29f4f3f0c003817d81680c95839
zeeshan-emumba <zeeshan.iqbal@emumba.com>
2020-11-05 15:39:20 +0500

    update celery branch and added tousif code for swagger

--- sampleproject/celery.py
+++ sampleproject/celery.py
@@ -4,10 +4,10 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "safekids.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sampleproject.settings")
 
 
-app = Celery("safekids")
+app = Celery("sampleproject")
 
 # Using a string here means the worker don't have to serialize
 # the configuration object to child processes.
